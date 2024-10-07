1a8ed99db8667b8451cbf556cc12ef389bd23f87
vuan201 <vuan201@gmail.com>
2024-10-07 09:10:04 +0700

    feat[profile address]: style address list

--- src/features/customer/Profile/Page/CustomerAddress/index.jsx
+++ src/features/customer/Profile/Page/CustomerAddress/index.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { useOutletContext } from "react-router";
+import { Container, CustomBox } from "../../../../../components";
+import { Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+const CustomerAddress = () => {
+  const { listAddress } = useOutletContext();
 
-const CustomerAddress = props => {
   return (
-    <div>
-      
-    </div>
+    <Container className={"space-y-4"}>
+      <CustomBox className={"flex justify-between px-8 py-4 items-center"}>
+        <span className="text-2xl font-light">Địa chỉ của tôi</span>
+        <Button variant="outlined" color="success" startIcon={<AddIcon />}>
+          Thêm địa chỉ mới
+        </Button>
+      </CustomBox>
+      {listAddress && listAddress.length > 0 ? (
+        listAddress.map((address) => (
+          <CustomBox
+            key={address.id}
+            className={"flex justify-between px-8 py-10 items-center"}
+          >
+            <div>
+              <div>{address.specificAddress}</div>
+              <div>
+                Phường {address.ward}, Quận {address.district}, {address.city}
+              </div>
+            </div>
+            <div className={"space-x-4"}>
+              <Button variant="outlined" color="info">
+                Cập nhật
+              </Button>
+              <Button variant="outlined" color="error">
+                Xóa
+              </Button>
+            </div>
+          </CustomBox>
+        ))
+      ) : (
+        <div className="text-center">Hiện không có địa chỉ nào</div>
+      )}
+    </Container>
   );
-};
-
-CustomerAddress.propTypes = {
-  
 };
 
 export default CustomerAddress;
